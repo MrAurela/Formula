56966df426f93e8e9c51658c0d8795573f87260b
aurelam1 <miroaurela@gmail.com>
2017-02-21 11:51:47 +0200

    Käyttöliittymän Grid ja Window luokat alustavasti tehty. Ikkunaan piirtyy ruudukko ja vaihteen vaihtamiseen tarkoitetut nappulat

---
+++ Formula/src/gui/Grid.scala
@@ -0,0 +1,21 @@
+package gui
+
+import scala.swing._
+
+class Grid(cols: Int, rows: Int) extends Panel {
+  
+  var squareSize = 50
+  
+  preferredSize = new Dimension(rows * squareSize + 1, cols * squareSize + 1 )
+  
+  override def paintComponent(g: Graphics2D) = {
+    this.drawLines(g)
+    this.drawTerrains(g)
+  }
+  
+  private def drawLines(g: Graphics2D) = {
+    for (col <- 0 to cols) g.drawLine(col * squareSize, 0, col * squareSize, rows * squareSize)
+    for (row <- 0 to rows) g.drawLine(0, row * squareSize, cols * squareSize, row * squareSize)
+  }
+  private def drawTerrains(g: Graphics2D) = println(cols, rows)
+}
--- Formula/src/gui/Window.scala
+++ Formula/src/gui/Window.scala
@@ -7,6 +7,8 @@ import javax.swing._
 /* Lähteet:
  * Pentaminoes-projektin (Ryhmämme työ Ohjelmointi 1 kurssilla) GameWindow-olio 
  * Ohjelmointi 1 kurssin materiaalin Swing-esimerkkipeli: sekä pdf että kooditiedostoista View-olio
+ * 
+ * BoxPanel(Orientation.Vertical) löydetty: http://stackoverflow.com/questions/13380701/scala-swing-component-alignment
  */
 
 
@@ -14,22 +16,55 @@ import javax.swing._
 object Window extends SimpleSwingApplication {
   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
   
-  val width = 640
-  val height = 480
+  val w = 1000
+  val h = 800
   
+  val vaihteenVaihto = new BoxPanel(Orientation.Horizontal) {
   
-  val gameScreen = new GridBagPanel {
-    ???
+    val vaihdeYlos = new Button {
+      preferredSize = new Dimension(100,100)
+      text = "->"
+    }
+    val vaihdeAlas = new Button {
+      preferredSize = new Dimension(100,100)
+      text = "<-"
+    }
+    val vaihdeLuku = new Button {
+      preferredSize = new Dimension(100,100)
+      text = "1"
+    }
+    
+    this.contents += vaihdeAlas
+    this.contents += vaihdeLuku
+    this.contents += vaihdeYlos
+    
   }
   
-  def top = new MainFrame {
+  
+  val vasenPuoli = new BoxPanel(Orientation.Vertical) {
+    val ruudukko = new Grid(10, 10)
+    this.contents += ruudukko
+    this.contents += vaihteenVaihto
+  }
+  
+  val ikkunanSisalto = new GridBagPanel {
+    val c = new Constraints
+    c.insets = new Insets(50,50,50,50)
+    layout(vasenPuoli) = c
+  }
+  
+  
+  val ikkuna = new MainFrame {
     title = "Formula"
-    preferredSize = new Dimension(width, height)
+    preferredSize = new Dimension(w, h)
     
-    contents = gameScreen
-    gameScreen.requestFocus
-    
-    
-
+    contents = ikkunanSisalto
+    //Tarvitaan vain jos käytetään näppäimistöä
+    //gameScreen.requestFocus
   }
+  
+  def top = ikkuna
+    
+    
+    
 }
